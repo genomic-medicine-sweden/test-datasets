beb8878de9270eb7708816a605ec869c60313125
Felix Lenner <felix.lenner@scilifelab.se>
2025-05-08 12:47:20 +0200

    Generalize script

---
+++ download_test_data.sh
@@ -0,0 +1,23 @@
+# Download somalier sites
+wget https://github.com/brentp/somalier/files/3412456/sites.hg38.vcf.gz -O data/sites.hg38.vcf.gz
+tabix data/sites.hg38.vcf.gz
+
+# Download CoLoRSdb 
+wget https://zenodo.org/records/11511513/files/CoLoRSdb.GRCh38.v1.0.0.pbsv.jasmine.vcf.gz -O data/CoLoRSdb.GRCh38.v1.0.0.pbsv.jasmine.vcf.gz
+wget https://zenodo.org/records/11511513/files/CoLoRSdb.GRCh38.v1.0.0.pbsv.jasmine.vcf.gz.tbi -O data/CoLoRSdb.GRCh38.v1.0.0.pbsv.jasmine.vcf.gz.tbi
+
+# Download reference genome
+wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/001/405/GCA_000001405.15_GRCh38/seqs_for_alignment_pipelines.ucsc_ids/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz -O data/GRCh38_no_alt_analysis_set.fasta.gz
+gunzip -c data/GRCh38_no_alt_analysis_set.fasta.gz > data/GRCh38_no_alt_analysis_set.fasta
+
+# Download ONT HG002 haplotagged bam
+#aws s3 cp s3://ont-open-data/giab_2023.05/analysis/variant_calling/hg002_sup_60x/hg002.haplotagged.bam data/hg002.haplotagged.bam --no-sign-request 
+#aws s3 cp s3://ont-open-data/giab_2023.05/analysis/variant_calling/hg002_sup_60x/hg002.haplotagged.bam.bai data/hg002.haplotagged.bam.bai --no-sign-request 
+
+# Add aligned PacBio HG002, HG003, HG004 manually, from Nallo run, as
+# data/HG002_aligned_haplotagged.bam
+# data/HG003_aligned_haplotagged.bam
+# data/HG004_aligned_haplotagged.bam
+# data/HG002_aligned_haplotagged.bam.bai
+# data/HG003_aligned_haplotagged.bam.bai
+# data/HG004_aligned_haplotagged.bam.bai
